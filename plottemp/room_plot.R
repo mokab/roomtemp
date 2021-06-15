0e562603dafc88638913888a3bb2e8fda473caba
mokab <masaaki.oka328@gmail.com>
2021-06-15 11:43:06 +0900

    Create room_plot.R

---
+++ plottemp/room_plot.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(scales)
+library(RSQLite)
+con = dbConnect(SQLite(), "~/Desktop/roomtemp.db", synchronous="off")
+res <- dbGetQuery(con, "select * from temp")
+plot(res$temp, type = 'l' )
+res$datetime <- as.POSIXct(res$datetime)
+
+p <- ggplot(res, aes(x = datetime, y = temp))
+p <- p + geom_line()
+p <- p + scale_x_datetime(date_breaks = "60 mins", 
+                          labels = date_format(format = "%d/ %H:%M", 
+                                               tz = "Asia/Tokyo"))
+plot(p)
